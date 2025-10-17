a3c07d1dc49917f674bcf47620a2c5fe5594b157
le50 <leti.tec1000@gmail.com>
2025-10-17 10:43:44 -0300

    Adicionado getters e setters

--- src/Atleta.java
+++ src/Atleta.java
@@ -2,4 +2,22 @@
 public class Atleta extends Pessoas{
 
 	String modalidade;
+	
+	
+	
+	public Atleta(int idPar, String nomePar, int idadePar, String modalidadePar){
+		super(idPar, nomePar, idadePar);
+		
+		this.modalidade = modalidadePar;
+		}
+	
+	
+	public String getModalidade(){
+		 return modalidade;
+	 }
+	 
+
+	 public void setModalidade(String modalidadePar){
+		 this.modalidade = modalidadePar;
+	 }
 }
--- src/Treinador.java
+++ src/Treinador.java
@@ -2,4 +2,19 @@
 public class Treinador extends Pessoas{
 
 	String especialidade;
+	
+	public Treinador(int idPar, String nomePar, int idadePar, String especialidadePar){
+		super(idPar, nomePar, idadePar);
+		
+		this.especialidade = especialidadePar;
+		}
+	
+		public String getEspecialidade(){
+			 return especialidade;
+		 }
+		 
+
+		 public void setEspecialidade(String especialidadePar){
+			 this.especialidade = especialidadePar;
+		 }
 }
